02e17e798e71b26eed665bf76fd4b4a23cd4a631
Anthony Quizon <anthony.quizon@smartsparrow.com>
2015-04-24 14:26:58 +1000

    added http server alias

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -30,3 +30,6 @@ export FLEX_HOME=$VENDOR_LIB/adobe-flex-sdk-4.6
 #PATH setup
 export PATH=$PACKER_HOME:$SCRIPTS_PATH:$VENDOR_PATH:$NPM_PACKAGES_PATH:$PATH
 
+
+alias serve='http-server -a localhost -p'
+alias emacs=emacs-24.4
