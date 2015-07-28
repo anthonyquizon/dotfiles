3a5a90f4fbf87da489abb7879f64afbaf7c3c9cf
Anthony Quizon <anthony.quizon@smartsparrow.com>
2015-07-28 10:51:40 +1000

    fixed: emacs mac paths

--- emacs/config/init.el
+++ emacs/config/init.el
@@ -1,7 +1,3 @@
-;; PATHS
-;;(when (memq window-system '(mac ns))
- ;; (exec-path-from-shell-initialize))
-
 (setenv "PATH" (concat (getenv "PATH") ":/Users/anthonyquizon/.npm-packages/bin"))
 (setq exec-path (append exec-path '("/Users/anthonyquizon/.npm-packages/bin")))
 (add-to-list 'exec-path "/usr/local/bin")
