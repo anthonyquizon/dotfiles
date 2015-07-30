c50331e585a32a923daafb51abf0b45ca6423613
Anthony Quizon <anthony.quizon@smartsparrow.com>
2015-07-30 18:07:33 +1000

    Autocomplete keybindings

--- emacs/config/packages/autocomplete.el
+++ emacs/config/packages/autocomplete.el
@@ -1,1 +1,4 @@
 (ac-config-default)
+
+(define-key ac-complete-mode-map (kbd "C-j") 'ac-next)
+(define-key ac-complete-mode-map (kbd "C-k") 'ac-previous)
--- emacs/config/packages/evil-leader-mode.el
+++ emacs/config/packages/evil-leader-mode.el
@@ -12,8 +12,22 @@
   "r" 'open-finder
   "g" 'magit-status
   "o" 'mode-line-other-buffer
+  
   "xf" 'find-file
   "xd" 'dired
+  
+  "wx" 'evil-window-delete
+  "ws" 'evil-window-split
+  "wv" 'evil-window-vsplit
+  "wl" 'evil-window-right
+  "wh" 'evil-window-left
+  "wk" 'evil-window-up
+  "wj" 'evil-window-down
+  "wL" 'evil-window-move-far-right
+  "wH" 'evil-window-move-far-left
+  "wK" 'evil-window-move-very-top
+  "wJ" 'evil-window-move-very-bottom
+  
   "<SPC>f" 'helm-projectile-find-file
   "<SPC>s" 'helm-projectile-ag
   "<SPC>d" 'helm-projectile-find-dir
