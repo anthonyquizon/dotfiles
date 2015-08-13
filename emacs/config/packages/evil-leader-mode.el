f6942bd2ccb300d6f978787e65f137065e5a6d75
Anthony Quizon <anthony.quizon@smartsparrow.com>
2015-08-13 15:06:57 +1000

    Space evil-leader for window keys

--- emacs/config/packages/evil-leader-mode.el
+++ emacs/config/packages/evil-leader-mode.el
@@ -20,6 +20,18 @@
   
   "cj" 'cider-jack-in
   
+  "wc" 'evil-window-delete
+  "ws" 'evil-window-split
+  "wv" 'evil-window-vsplit
+  "wl" 'evil-window-right
+  "wh" 'evil-window-left
+  "wk" 'evil-window-up
+  "wj" 'evil-window-down
+  "wL" 'evil-window-move-far-right
+  "wH" 'evil-window-move-far-left
+  "wK" 'evil-window-move-very-top
+  "wJ" 'evil-window-move-very-bottom
+  
   "<SPC>f" 'helm-projectile-find-file
   "<SPC>s" 'helm-projectile-ag
   "<SPC>d" 'helm-projectile-find-dir
