0fc948bf727240799a973d0c6ec9d8a86896a6c5
Anthony Quizon <anthony.quizon@smartsparrow.com>
2015-08-20 10:23:29 +1000

    C-j and C-k for company mode

---
+++ emacs/config/packages/company-mode.el
@@ -0,0 +1,5 @@
+(global-company-mode)
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-j") #'company-select-next)
+  (define-key company-active-map (kbd "C-k") #'company-select-previous))
