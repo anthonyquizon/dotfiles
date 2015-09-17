85b2f8dae86802cc456e9de8624b5684e4e20b87
Anthony Quizon <anthoq88@gmail.com>
2015-09-18 07:02:13 +1000

    smart parens and gitconfig

--- emacs/config/packages/evil-smartparens.el
+++ emacs/config/packages/evil-smartparens.el
@@ -10,3 +10,16 @@
 (add-hook 'lisp-interaction-mode-hook #'smartparens-strict-mode)
 (add-hook 'scheme-mode-hook           #'smartparens-strict-mode)
 
+;;sp-unwrap-sexp
+;; "pu" 'sp-unwrap-sexp
+;; "pk" 'sp-add-to-previous-sexp
+;; "pj" 'sp-unwrap-sexp
+
+
+(define-key smartparens-mode-map (kbd "C-M-<backspace>") 'sp-unwrap-sexp)
+;; (define-key smartparens-mode-map (kbd "C-<backspace>") 'sp-down-sexp)
+
+(define-key smartparens-mode-map (kbd "C-j") 'sp-down-sexp)
+(define-key smartparens-mode-map (kbd "C-k") 'sp-up-sexp)
+(define-key smartparens-mode-map (kbd "C-M-J") 'sp-add-to-next-sexp)
+(define-key smartparens-mode-map (kbd "C-M-K") 'sp-add-to-previous-sexp)
