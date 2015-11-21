4ad4e172b8bf1ceea835a43004b8b397a147eb5d
Anthony Quizon <anthoq88@gmail.com>
2015-11-22 10:59:42 +1100

    removed evil-mode copy on highlight

--- emacs/config/packages/evil-mode.el
+++ emacs/config/packages/evil-mode.el
@@ -57,3 +57,12 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (global-set-key [escape] 'evil-exit-emacs-state)
 
 
+; Override the default x-select-text function because it doesn't
+; respect x-select-enable-clipboard on OS X.
+(defun x-select-text (text))
+(setq x-select-enable-clipboard nil)
+(setq x-select-enable-primary nil)
+(setq mouse-drag-copy-region nil)
+
+(setq interprogram-cut-function 'ns-set-pasteboard)
+(setq interprogram-paste-function 'ns-get-pasteboard)
--- setup.hy
+++ setup.hy
@@ -1,13 +1,9 @@
 #!/usr/bin/env hy
 
-(import shutil)
+(import plumbum)
 (import os)
 
 ;; TODO remove all files from HOME dir that matches the files in the dotfile folder
-;; TODO syslink everything in dotfiles folder to HOME dir
+;; TODO sys everything in dotfiles folder to HOME dir
 
-(defn clean []
-  ;; remove for each folder/file in 
-  (if (or (os.path.isdir dest-dir) (os.path.lexists dest-dir))
-    (do (print "removing " dest-dir)
-        (os.remove dest-dir))))
+
