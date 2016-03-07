b2b948783eb0b4274e24667f5eabf06976b1b201
Anthony Quizon <anthoq88@gmail.com>
2016-03-07 15:19:44 +1100

    Platform path spr

--- bash/spr.sh
+++ bash/spr.sh
@@ -1,3 +1,4 @@
+export PLATFORM=~/Development/Projects/platform/core/
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export ANT_HOME=$VENDOR_LIB/apache-ant-1.9.4
 export TOMCAT_HOME=$VENDOR_LIB/apache-tomcat-7.0.56
