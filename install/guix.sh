6cc4de49844fc069f0616bdbdae5d34f371ada70
Anthony Quizon <accounts@anthonyquizon.io>
2020-04-22 08:06:38 +0000

    guix path

--- install/guix.sh
+++ install/guix.sh
@@ -1,1 +1,1 @@
-guix install tmux make git guile ripgrep iptables nss-certs
+guix install tmux make git guile ripgrep iptables nss-certs node
---
+++ install/npm.sh
@@ -0,0 +1,2 @@
+mkdir "${HOME}/.npm-packages"
+npm config set prefix "${HOME}/.npm-packages"
