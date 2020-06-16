49aa922d361046f32658ee207c38ce6975422a17
Anthony Quizon <accounts@anthonyquizon.io>
2020-06-16 23:04:35 +0000

    ufw defaults

--- install/ufw.sh
+++ install/ufw.sh
@@ -1,6 +1,8 @@
 read ipaddress
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install ufw
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
 sudo ufw allow ssh
 sudo ufw allow from $ipaddress
 sudo ufw enable
