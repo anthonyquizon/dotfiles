0a1cde9c7890fb9d246d1c766f8c38463bad1581
Anthony Quizon <accounts@anthonyquizon.io>
2020-06-16 23:07:17 +0000

    ufw confirmation messge

--- install/ufw.sh
+++ install/ufw.sh
@@ -1,10 +1,14 @@
-read ipaddress
+read -p "What's your ip address?" ipaddress
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install ufw
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
 sudo ufw allow ssh
 sudo ufw allow from $ipaddress
-sudo ufw enable
+sudo ufw status
 
+read -p "Is this ok? (yes/no): " confirm
 
+if [ $confirm == "yes" ]; then
+    sudo ufw enable
+fi
