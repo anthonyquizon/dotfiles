6f5d8f41b64b9070824a66ef3de76f3b6f977f52
Anthony Quizon <accounts@anthonyquizon.io>
2020-06-22 04:56:44 +0000

    change theme on guix env

--- install/guix.sh
+++ install/guix.sh
@@ -1,5 +1,5 @@
 guix pull
-guix install tmux make git guile ripgrep iptables nss-certs node nss-certs
+guix install tmux make git guile ripgrep iptables nss-certs node nss-certs readline guile guile-readline
 
 git clone git@github.com:anthonyquizon/guix-channel.git $HOME/guix-channel
 
