529f948efe32c858eb495609e62101939859804c
Anthony Quizon <accounts@anthonyquizon.io>
2020-08-21 04:47:58 +0000

    removed node; added make -j

--- install/guix.sh
+++ install/guix.sh
@@ -1,5 +1,15 @@
 guix pull
-guix install tmux make git guile ripgrep iptables nss-certs node nss-certs readline guile guile-readline
+guix install \
+    tmux \
+    make \
+    git \
+    guile \
+    ripgrep \
+    iptables \
+    nss-certs \
+    readline \
+    guile \
+    guile-readline
 
 git clone git@github.com:anthonyquizon/guix-channel.git $HOME/guix-channel
 
