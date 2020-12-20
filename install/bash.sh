267beb8fb7272125f840c663877d26f9f279aef0
Anthony Quizon <accounts@anthonyquizon.io>
2020-12-20 21:50:46 +0000

    removed oh my bash

--- install/bash.sh
+++ install/bash.sh
@@ -3,5 +3,3 @@
 export SSL_CERT_DIR="$HOME/.guix-profile/etc/ssl/certs"
 export SSL_CERT_FILE="$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt"
 export GIT_SSL_CAINFO="$SSL_CERT_FILE"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
