43fb95e992826739a1af444668c5bbdea3204e6b
Anthony Quizon <accounts@anthonyquizon.io>
2021-07-29 01:50:17 +0000

    dlang unicode mappings

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 dotfiles=$HOME/dotfiles/dotfiles
 
 mkdir -p $HOME/.config/nvim/
-mkdir -p $HOME/.config/guix/channels.scm
+mkdir -p $HOME/.config/guix/
 
 ln -sf $dotfiles/bashrc $HOME/.zprofile
 ln -sf $dotfiles/bashrc $HOME/.bash_profile
 ln -sf $dotfiles/bashrc $HOME/.bashrc
 ln -sf $dotfiles/vimrc $HOME/.vimrc
 ln -sf $dotfiles/vimrc $HOME/.config/nvim/init.vim
+ln -sf $dotfiles/vim/ftplugin $HOME/.config/nvim/ftplugin
 ln -sf $dotfiles/mongorc.js $HOME/.mongorc.js
 ln -sf $dotfiles/gitconfig $HOME/.gitconfig
 ln -sf $dotfiles/tmux.conf $HOME/.tmux.conf
