fd0ddab99df392cdb79937a9a328441c902fc14d
Anthony Quizon <anthonyquizon@Anthonys-MacBook.local>
2025-04-16 16:47:09 +1000

    minimal vim

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 Antho's config files
 
 
-## SSH certificate
-* Steps (on local machine): 
-    * `ssh-keygen -b 4096` 
-    * `ssh-copy-id <username>@<ipaddress>`
-* Reference: https://www.linode.com/docs/security/authentication/use-public-key-authentication-with-ssh/
--- setup.sh
+++ setup.sh
@@ -1,21 +1,5 @@
 dotfiles=$HOME/dotfiles/dotfiles
 
-#tmux
-if [ ! -d ~/.tmux-themepack ]; then
-    git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
-fi
-
 #vim
 mkdir -p $HOME/.vim/undodir
-
-# install plug in neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-ln -sf $dotfiles/bashrc $HOME/.zprofile
-ln -sf $dotfiles/bashrc $HOME/.bash_profile
-ln -sf $dotfiles/bashrc $HOME/.bashrc
 ln -sf $dotfiles/vimrc $HOME/.vimrc
-ln -sf $dotfiles/alacritty.yml $HOME/.config/alacritty.yml
-ln -sf $dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $dotfiles/tmux.conf $HOME/.tmux.conf
